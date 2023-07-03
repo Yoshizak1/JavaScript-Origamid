e83524251762d65f956d38c36b08e732161f9b15
João Pedro Yoshizaki <Yoshizaki02@hotmail.com>
2023-07-03 11:01:35 -0300

    tipo-dados

--- Tipos-de-Dados/script.js
+++ Tipos-de-Dados/script.js
@@ -8,10 +8,24 @@ var nomeCompleto = nome + sobrenome;
 
 console.log(nomeCompleto);
 
-var gols = 1000;
-var frase = 'Romário fez' + gols + 'gols';
-console.log(typeof frase);
+// var gols = 1000;
+// var frase = 'Romário fez' + gols + 'gols';
+// console.log(typeof frase);
 
 var ano 2018;
 var mes = 8;
 console.log(ano + mes);
+
+
+var melhor = 'teste'
+var frase1 = "Esse é o" + melhor + "jogo";
+var frase2 = "Esse é o  \"melhor\" jogo";
+console.log(frase1);
+console.log(frase2);
+
+var gols = 1000;
+var frase3 = 'Romário fez' + gols + 'gols';
+var frase4 = `Romário fez ${gols} gols`;
+
+
+console.log(frase4);
