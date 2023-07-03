4a89ebe0d371e572b45573a733ab1952c40aaff5
João Pedro Yoshizaki <Yoshizaki02@hotmail.com>
2023-07-03 10:48:19 -0300

    commit inicial

---
+++ Tipos-de-Dados/script.js
@@ -0,0 +1,17 @@
+var idade = 28;
+var simbolo = Symbol();
+console.log(typeof nome);
+
+var nome = 'André';
+var sobrenome = 'Rafael';
+var nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = 'Romário fez' + gols + 'gols';
+console.log(typeof frase);
+
+var ano 2018;
+var mes = 8;
+console.log(ano + mes);
---
+++ Variaveis/script.js
@@ -0,0 +1,39 @@
+var nome = 'André';
+var idade = 28;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Rafael', 
+    cidade = 'Rio';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+var comida;
+console.log(comida);
+comida = 'Pizza';
+console.log(comida);
+
+var time = 'Vasco';
+time = 'Flamengo';
+
+console.log(time);
+
+// Exercicio
+
+let meuNome = 'João';
+let minhaIdade = 19;
+let comidaFavorita;
+comidaFavorita = 'Pizza';
+let var1, var2, var3, var4, var5;
+
+console.log(meuNome, minhaIdade, comidaFavorita);
