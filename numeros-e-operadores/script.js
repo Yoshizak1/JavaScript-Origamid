0221c16bc719a5d79b5611d3bbb358d83ebdf3ca
João Pedro Yoshizaki <Yoshizaki02@hotmail.com>
2023-07-03 14:23:24 -0300

    numeros-operadores-2

--- numeros-e-operadores/script.js
+++ numeros-e-operadores/script.js
@@ -5,3 +5,15 @@ console.log(soma);
 
 var testeNaN = 'Isso é 100' / 2;
 console.log(testeNaN);
+
+var soma1 = 10 + 10 + 20 + 30 *4 / 2 + 10;
+console.log(soma1);
+
+var x = 5;
+console.log(++x);
+console.log(x);
+
+var idade = 28;
+var somaIdade = 5;
+
+console.log(+idade + -somaIdade);
