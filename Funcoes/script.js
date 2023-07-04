d9ae175dd74f3eda9cdc74884c6000216d239eb0
João Pedro Yoshizaki <Yoshizaki02@hotmail.com>
2023-07-04 10:18:34 -0300

    funcoes-2

--- Funcoes/script.js
+++ Funcoes/script.js
@@ -30,3 +30,40 @@ function corFavorita(cor) {
 addEventListener('click', function() {
   console.log('Oi');
 })
+
+function imc2(peso, altura) {
+  const imc = peso / (altura ** 2);
+  console.log(imc2);
+}
+
+function terceiraIdade(idade) {
+  console.log(typeof idade);
+  if (typeof idade !== 'number') {
+    return 'Por favor preencha um número';
+  }
+  if(idade >= 60) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(terceiraIdade(60));
+
+function faltaVisitar(paisesVisitados) {
+  var totalPaises = 193;
+  return `Faltam visitar ${totalPaises - paisesVisitados} países`;
+}
+
+function dados() {
+  var nome = 'João';
+  var idade = 28;
+  function outrosDados() {
+    var endereco = 'São Paulo';
+    var idade = 29;
+    return `${nome}, ${idade}, ${endereco}, ${profissao}`;
+  }
+  return outrosDados();
+}
+
+console.log(dados());
