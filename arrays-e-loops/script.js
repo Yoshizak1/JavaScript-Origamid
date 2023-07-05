d086e4c83343dc650a678a6bb967c98adf8f2a91
João Pedro Yoshizaki <Yoshizaki02@hotmail.com>
2023-07-05 15:23:05 -0300

    arrays-e-loops

--- arrays-e-loops/script.js
+++ arrays-e-loops/script.js
@@ -1,15 +1,34 @@
-var videoGames = ['Switch', 'PS4', 'Xbox'];
-
-var ultimoItem = videoGames.pop();
-
-videoGames.push('3DS');
+//var ultimoItem = videoGames.pop();
 
 for (var numero = 0; numero < 10; numero++) {
   console.log(numero);
 }
 
 var i = 0;
-while (i < 100) {
+while (i < 10) {
   console.log(i);
   i++;
 }
+
+var videoGames = ['Switch', 'PS4', 'Xbox', '3DS'];
+
+for(var item = 0; item < videoGames.length; item++) {
+  console.log(videoGames[item]);
+  if(videoGames[item] === 'PS4') {
+    break;
+  }
+}
+
+var frutas = ['banana', 'pera', 'maça', 'abacaxi'];
+
+videoGames.forEach(function(fruta, index, array) {
+  console.log(fruta, index, array);
+})
+
+var numero = 0;
+var maximo = 50;
+for(;numero < maximo;) {
+  console.log(numero);
+  numero++;
+}
+
