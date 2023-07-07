a89ac222d58f33c070e9b79b87255ce8b67e4e81
João Pedro Yoshizaki <Yoshizaki02@hotmail.com>
2023-07-07 10:43:42 -0300

    o-que-e-o-dom-2

--- dom-para-iniciantes/o-que-e-o-dom/script.js
+++ dom-para-iniciantes/o-que-e-o-dom/script.js
@@ -5,3 +5,10 @@ if(href === '') {
 }
 
 const h1Selecionado = document.querySelector('h1');
+const h1Classes = h1Selecionado.classList;
+
+function callBackh1() {
+  console.log('Clicou em' h1Selecionado.innerText);
+}
+
+h1Selecionado.addEventListener('click', callBackh1);
