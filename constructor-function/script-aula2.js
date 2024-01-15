a99534beb3f96407d71795592411eb22c3735455
João Pedro Yoshizaki dos Santos <yoshizakix@outlook.com>
2024-01-15 17:15:48 -0300

    constructor

--- constructor-function/script-aula2.js
+++ constructor-function/script-aula2.js
@@ -1,24 +1,11 @@
-// const Dom = {
-//   seletor: 'li',
-//   element() {
-//     return document.querySelector(this.seletor);
-//   },
-//   ativar() {
-//     this.element().classList.add('ativar');
-//   }
-// }
+const honda = new Carro('Honda', 3000);
+const fiat = new Carro('Fiat', 4000);
 
-function Dom(seletor) {
-  this.element = function() {
-    return document.querySelector(seletor);
-  }
-  this.ativar = function(classe) {
-    this.element().classList.add(classe);
-  }
+function Carro2(marca, precoInicial) {
+  const taxa = 1.2;
+  const precoFinal = precoIncial * taxa;
+  this.marca = marca;
+  this.preco = precoFinal;
 }
 
-const li = new Dom('li');
-const ul = new Dom('ul');
-
-const lastLi = new Dom('li:last-child');
-lastLi.ativar('ativar');
+const mazda = new Carro2('Mazda', 5000);
