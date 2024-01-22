aa879e76ae4907d51b304b830290905aed4133ee
João Pedro Yoshizaki dos Santos <yoshizakix@outlook.com>
2024-01-22 16:39:58 -0300

    number-math-1-2

--- number-math/script.js
+++ number-math/script.js
@@ -2,3 +2,26 @@
 console.log(Number.isNaN('ds'));
 // isInteger
 console.log(Number.isInteger(10.4343434));
+
+//parseFloat e parseInt
+console.log(parseFloat('3224523421'));
+console.log(parseFloat('100 reais'));
+console.log(parseInt('100.27'));
+
+//toFixed
+const preco = 2.99;
+console.log(preco.toFixed());   
+
+//toString
+const preco = 2.99;
+preco.toString(10);
+
+//toLocaleString(lang, options)
+
+const valor = 48.49;
+valor = valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+console.log(valor);
+
+//Math
+
+console.log(Math.PI);
